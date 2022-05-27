a55918deea1bb25f6280baae55247c57204d9eae
manish karki <karkim452@gmail.com>
2022-05-27 07:57:15 +0545

    learn about datatype

--- consoleapp/Program.cs
+++ consoleapp/Program.cs
@@ -1,2 +1,14 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
+byte age = 23; //0.225
+short b = 3534;
+int c = 2523523;
+long d = 32352253225;
+float e = 2342.546516516f;
+double f = 23425.56165161616161;
+decimal g = 34.5615156156156156165m;
+bool h = true;
+
+char i = 'N';
+string j = "chfcgc;df;vhb";
+int? value = null;
